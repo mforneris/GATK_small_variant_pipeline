a072b27a3c832d4d58e710c750d3ab0530555866
Mattia Forneris <mattia.forneris@embl.de>
2019-09-05 16:28:05 +0200

    Complete project
    On the final commit, some folders were missing. Here is the complete project

---
+++ src/snakemake/QUALITY_CONTROL/snakemake_default.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+
+/g/furlong/forneris/software/anaconda3/bin/snakemake -j 50 --use-conda --cluster-config cluster.json --configfile ../../../config/config.yml --cluster "{cluster.sbatch} -p {cluster.partition} --cpus-per-task {cluster.n} --mem {cluster.mem} {cluster.moreoptions}" --timestamp --keep-going --rerun-incomplete
